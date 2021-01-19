6367e05bd9a7dde233d73158711c5eef745e9e09
snowiewdev <snowiewongdev@gmail.com>
2021-01-20 00:09:52 +0800

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p> Welcome to Westworld Circle </p>
+      <p> Created by SnowieW after watching WestWorld </p>
+    </div>
+  )
+}
+
+export default Footer;
