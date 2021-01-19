caf438db76ea8768548114afef9eaaea6dc76ad3
snowiewdev <snowiewongdev@gmail.com>
2021-01-20 00:42:42 +0800

    footer css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ function Home(){
 
   return(
     <div>
-      <Grid stackable columns={3}>
+      <Grid stackable columns={3} className="content-container">
         <Grid.Row className="page-title">
           <h1> Recent Posts </h1>
         </Grid.Row>
